c4d5796b4350cc2470d74afb14a2b7b4fabdbaf8
Hectorcrazy <94460102+hectorcrazy@users.noreply.github.com>
2022-05-07 18:45:39 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo "basado en debian 11\n Slax Linux una distro linux desde tu bolsillo"
+sleep 3
+echo "4GB/ 8GB changes.dat"
+sleep 3
+echo "El sudo no hace falta B)"
+apt install xfce4
+apt install xfce4-goodies
+apt install codeblocks
+apt install mame
+apt install gimp
